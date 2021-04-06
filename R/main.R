c95bc25599fe96a0ec3182738b92bc1fc449d082
Massimo Andreatta <massimo.andreatta@unil.ch>
2021-04-06 11:28:27 +0200

    Migrate from GitLab

---
+++ R/main.R
@@ -0,0 +1,157 @@
+#' Calculate module enrichment scores using Mann-Whitney U statistic from single-cell data (Seurat interface)
+#'
+#' Given a Seurat object, calculates module/signature enrichment scores on single-cell level using Mann-Whitney U statistic.
+#' Returned scores are normalized U statistic (equivalent to AUC - Area Under the Curve).
+#' In contrast to Seurat's AddModuleScore (based on population average gene expression binning) this score depend only on the gene expression ranks of individual cell, and therefore is robust across datasets.
+#'
+#' @param object Seurat object
+#' @param features A list of signatures, for example: \code{list( Tcell_signature = c("CD2","CD3E","CD3D"), Myeloid_signature = c("SPI1","FCER1G","CSF1R"))}
+#' @param chunk.size Number of cells to be processed simultaneously (lower size requires slightly more computation but reduces memory demands)
+#' @param maxRank Maximum number of genes to rank per cell; above this rank, a given gene is considered as not expressed.
+#'     Note: this parameter is ignored if precalc.ranks are specified
+#' @param ncores Number of processors to parallelize computation. Requires package \code{future}
+#' @param storeRanks Store ranks matrix in Seurat object (@misc slot) for fast subsequent computations. This option will demand large amounts of RAM.
+#' @param assay Pull out data from this assay of the Seurat object (if NULL, use \code{DefaultAssay(obj)})
+#' @param slot Pull out data from this slot of the Seurat object
+#' @param force.gc Explicitly call garbage collector to reduce memory footprint
+#' @param seed Integer seed for 'future' parallel execution
+#' @param name Name tag that will be appended at the end of each signature name, "_UCell" by default (e.g. signature score in meta data will be named: Tcell_signature_UCell)
+#' @return Returns a Seurat object with module/signature enrichment scores added to object meta data; each score is stored as the corresponding signature name provided in \code{features} followed by the tag given in \code{name} (or "_UCell" by default )
+#' @examples
+#' ## Not run:
+#' library(UCell)
+#' SeuratObject <- AddModuleScore_UCell(SeuratObject,features=list( Tcell_signature = c("CD2","CD3E","CD3D"), Myeloid_signature = c("SPI1","FCER1G","CSF1R")))
+#' SeuratObject$Tcell_signature_UCell
+#' head(SeuratObject@meta.data)
+#' ## End (Not run)
+#' @export
+AddModuleScore_UCell <- function(obj, features, maxRank=1500, chunk.size=1000, ncores=1, storeRanks=F, 
+                                 assay=NULL, slot="data", force.gc=FALSE, seed=123, name="_UCell") {
+
+  if (ncores>1) {
+    require(future.apply)
+    future_param_seed <<- seed
+    future_param_ncores <<- ncores
+  }
+  
+  features <- check_signature_names(features)
+  
+  if (is.null(assay)) {
+     assay <- Seurat::DefaultAssay(obj)
+  }
+  precomputedRanks <- obj@misc[["UCell"]][[assay]][["cells_rankings"]]
+
+  #If rank matrix was pre-computed, evaluate the new signatures from these ranks
+  #Else, calculate new ranks to score signatures (optionally storing ranks, takes up memory but become very fast to evaluate further signatures)
+  if (!is.null(precomputedRanks)) {
+    meta.list <- rankings2Uscore(precomputedRanks, features=features, chunk.size=chunk.size, 
+                                 ncores=ncores, force.gc=force.gc, name=name)
+
+  } else {
+    meta.list <- calculate_Uscore(obj@assays[[assay]][[slot]], features=features, maxRank=maxRank, chunk.size=chunk.size, 
+                                  ncores=ncores, force.gc=force.gc, storeRanks=storeRanks, name=name)
+
+    #store ranks matrix?
+    if (storeRanks==T){
+      cells_rankings.merge <- lapply(meta.list,function(x) rbind(x[["cells_rankings"]]))
+      cells_rankings.merge <- Reduce(cbind, cells_rankings.merge)
+
+      obj@misc[["UCell"]][[assay]] <- list(cells_rankings=cells_rankings.merge)
+    }
+  }
+
+  meta.merge <- lapply(meta.list,function(x) rbind(x[["cells_AUC"]]))
+  meta.merge <- Reduce(rbind, meta.merge)
+  
+  obj <- Seurat::AddMetaData(obj, as.data.frame(meta.merge))
+
+  return(obj)
+}
+
+#' Calculate module enrichment scores using Mann-Whitney U statistic from single-cell data
+#'
+#' Given a gene vs. cell matrix, calculates module/signature enrichment scores on single-cell level using Mann-Whitney U statistic.
+#' Returned scores are normalized U statistic (equivalent to AUC - Area Under the Curve).
+#' This score depends only on the gene expression ranks of individual cell, and therefore is robust across datasets.
+#'
+#' @param matrix A gene vs. cell data matrix, either in sparse or dense format. Leave empty if providing a rank matrix with \code{precalc.ranks}
+#' @param features A list of signatures, for example: \code{list( Tcell_signature = c("CD2","CD3E","CD3D"), Myeloid_signature = c("SPI1","FCER1G","CSF1R"))}
+#' @param precalc.ranks A sparse matrix of pre-calculated ranks, obtained with \code{\link{StoreRankings_UCell}}
+#' @param maxRank Maximum number of genes to rank per cell; above this rank, a given gene is considered as not expressed.
+#'     Note: this parameter is ignored if precalc.ranks are specified
+#' @param chunk.size Number of cells to be processed simultaneously (lower size requires slightly more computation but reduces memory demands)
+#' @param ncores Number of processors to parallelize computation. Requires package \code{future}
+#' @param force.gc Explicitly call garbage collector to reduce memory footprint
+#' @param seed Integer seed for 'future' parallel execution
+#' @return Returns a dataframe of signature scores for each cell
+#' @examples
+#' ## Not run:
+#' library(UCell)
+#' scores <- ScoreSignatures_UCell(expression_matrix,features=list( Tcell_signature = c("CD2","CD3E","CD3D"), Myeloid_signature = c("SPI1","FCER1G","CSF1R")))
+#' scores[1:5,1:5]
+#' ## End (Not run)
+#' @export
+ScoreSignatures_UCell <- function(matrix=NULL, features, precalc.ranks=NULL, maxRank=1500, 
+                                  chunk.size=1000, ncores=1, force.gc=FALSE, seed=123) {
+  
+  if (ncores>1) {
+    require(future.apply)
+    future_param_seed <<- seed
+    future_param_ncores <<- ncores
+  }
+  features <- check_signature_names(features)
+  
+  if (!is.null(precalc.ranks)) {
+     meta.list <- rankings2Uscore(precalc.ranks, features=features, chunk.size=chunk.size, 
+                                  ncores=ncores, force.gc=force.gc)
+  } else {
+     meta.list <- calculate_Uscore(matrix, features=features, maxRank=maxRank, chunk.size=chunk.size, 
+                                   ncores=ncores, force.gc=force.gc)
+  }
+  meta.merge <- lapply(meta.list,function(x) rbind(x[["cells_AUC"]]))
+  meta.merge <- Reduce(rbind, meta.merge)
+  
+  return(meta.merge)
+
+}
+
+#' Calculate and store gene rankings for a single-cell dataset
+#'
+#' Given a gene vs. cell matrix, calculates the rankings of expression for all genes in each cell. It can then be applied to the
+#' function \code{\link{ScoreSignatures_UCell}} to evaluate gene signatures on the gene expression ranks of individual cells.
+#'
+#' @param matrix A gene vs. cell data matrix, either in sparse or dense format
+#' @param maxRank Maximum number of genes to rank per cell; above this rank, a given gene is considered as not expressed
+#' @param chunk.size Number of cells to be processed simultaneously (lower size requires slightly more computation but reduces memory demands)
+#' @param ncores Number of processors to parallelize computation. Requires package \code{future}
+#' @param force.gc Explicitly call garbage collector to reduce memory footprint
+#' @param seed Integer seed for 'future' parallel execution
+#' @return Returns a sparse matrix of pre-calculated ranks that can be used multiple times to evaluate different signatures
+#' @examples
+#' ## Not run:
+#' library(UCell)
+#' ranks <- StoreRankings_UCell(expression_matrix)
+#' ranks[1:5,1:5]
+#' scores <- ScoreSignatures_UCell(expression_matrix,features=feature.list, precalc.ranks=ranks)
+#' ## End (Not run)
+#' @export
+StoreRankings_UCell <- function(matrix, maxRank=1500, chunk.size=1000, ncores=1, force.gc=FALSE, seed=123) {
+  
+  if (ncores>1) {
+    require(future.apply)
+    future_param_seed <<- seed
+    future_param_ncores <<- ncores
+  }
+  
+  features <- rownames(matrix)[1]  #dummy signature
+  meta.list <- calculate_Uscore(matrix, features=features, maxRank=maxRank, chunk.size=chunk.size, 
+                                ncores=ncores, storeRanks=T, force.gc=force.gc)
+  
+  ranks.all <- lapply(meta.list,function(x) rbind(x[["cells_rankings"]]))
+  ranks.all <- Reduce(cbind, ranks.all)
+  
+  return(ranks.all)
+
+}
+
+
---
+++ R/utils.R
@@ -0,0 +1,236 @@
+
+#Calculate AUC as Mann–Whitney U statistic from a vector of ranks
+u_stat <- function(rank_value, maxRank=1000, sparse=F){
+
+  if(sparse==T){
+    rank_value[rank_value==0] <- maxRank+1
+  }
+
+  insig <- rank_value > maxRank
+  if(all(insig)) {
+    return(0L)
+  } else {
+    rank_value[insig] <- maxRank+1
+    rank_sum = sum(rank_value)
+    len_sig <- length(rank_value)
+    u_value = rank_sum - (len_sig * (len_sig + 1))/2
+    auc = u_value/(len_sig * maxRank)
+    auc = 1 - auc
+    return(auc)
+  }
+}
+
+#Check if all genes in signatures are found in data matrix - otherwise add zero counts in data-matrix to complete it
+check_genes <- function(matrix, features) {
+   features <- unlist(features)
+   missing <- setdiff(features, rownames(matrix))
+   ll <- length(missing)
+   
+   if (ll/length(features) > 0.5) {
+      warning(sprintf("Over half of genes (%s%%) in specified signatures are missing from data. Check the integrity of your dataset\n", round(100*ll/length(features))))
+   }
+   
+   if (ll>0) {
+      add.mat <- Matrix::sparseMatrix(length(missing), ncol(matrix))
+      rownames(add.mat) <- missing
+      matrix <- rbind(matrix, add.mat)
+      
+      missing.concatenate <- paste(missing, collapse=",")
+      warning(sprintf("The following genes were not found and will be imputed to exp=0:\n* %s",missing.concatenate))
+   }
+   return(matrix)
+}
+
+#Check signature names and add standard names is missing
+
+check_signature_names <- function(features) {
+  defaultSigName <- paste0(rep("signature_",length(features)),seq_along(features))
+  if(is.null(names(features))){
+    names(features) <- defaultSigName
+  } else {
+    invalidNames <- names(features) == "" | duplicated(names(features))
+    names(features)[invalidNames] <- defaultSigName[invalidNames]
+  }
+  return(features)
+}
+
+#Calculate AUC for a list of signatures, from a ranks matrix
+u_stat_signature_list <- function(sig_list, ranks_matrix, maxRank=1000, sparse=F) {
+
+  u_matrix <- sapply(sig_list, function(sig) {
+    as.numeric(ranks_matrix[sig, lapply(.SD, function(x) u_stat(x,maxRank = maxRank,sparse=sparse)),.SDcols=-1, on="rn"])
+    })
+
+  rownames(u_matrix) <- colnames(ranks_matrix)[-1]
+  return (u_matrix)
+}
+
+# Calculate features' ranks from expression data matrices
+data_to_ranks_data_table = function(data) {
+  dt <- as.data.table(as.matrix(data))
+  rnaDT.ranks.dt <- dt[, lapply(.SD, function(x) frankv(x,ties.method="random",order=c(-1L)))]
+  rnaDT.ranks.rownames <- rownames(data)
+  rnaDT.ranks.dt.rn <- cbind(rn=rnaDT.ranks.rownames, rnaDT.ranks.dt)
+  setkey(rnaDT.ranks.dt.rn, rn, physical = F)
+  return(rnaDT.ranks.dt.rn)
+}
+
+#split data matrix into cell chunks
+split_data.matrix <- function(matrix, chunk.size=1000) {
+  ncols <- dim(matrix)[2]
+  nchunks <- (ncols-1) %/% chunk.size + 1
+
+  split.data <- list()
+  for (i in 1:nchunks) {
+    min <- 1 + (i-1)*chunk.size
+    max <- min(i*chunk.size, ncols)
+    split.data[[i]] <- matrix[,min:max]
+  }
+  return(split.data)
+}
+
+#Get signature scores from precomputed rank matrix
+
+rankings2Uscore <- function(ranks_matrix, features, chunk.size=1000, 
+                            ncores=1, force.gc=FALSE, name="_UCell") {
+
+  #Check if all genes in signatures are present in the stored signatures
+  ranks_matrix <- check_genes(ranks_matrix, features)
+
+  maxRank <- max(ranks_matrix)
+  split.data <- split_data.matrix(matrix=ranks_matrix, chunk.size=chunk.size)
+  rm(ranks_matrix)
+
+  if (ncores>1) {
+    plan(future::multisession(workers=future_param_ncores))
+
+    meta.list <- future_lapply(
+      X = split.data,
+      FUN = function(x) {
+
+        dense <- as.matrix(x)
+        dense <- as.data.table(dense, keep.rownames=TRUE)
+        setkey(dense, rn, physical=F)
+
+        cells_AUC <- u_stat_signature_list(features, dense, maxRank=maxRank, sparse=T)
+        colnames(cells_AUC) <- paste0(colnames(cells_AUC),name)
+        
+        if (force.gc) {
+          dense <- NULL
+          gc()
+        }
+        return(list(cells_AUC=cells_AUC))
+      },
+      future.seed = future_param_seed
+    )
+    plan(strategy = "sequential")
+
+  } else {
+
+    meta.list <- lapply(
+      X = split.data,
+      FUN = function(x) {
+
+        dense <- as.matrix(x)
+        dense <- as.data.table(dense, keep.rownames=TRUE)
+        setkey(dense, rn, physical=F)
+
+        cells_AUC <- u_stat_signature_list(features, dense, maxRank=maxRank, sparse=T)
+        colnames(cells_AUC) <- paste0(colnames(cells_AUC),name)
+        
+        if (force.gc) {
+          dense <- NULL
+          gc()
+        }
+
+        return(list(cells_AUC=cells_AUC))
+      } )
+  }
+  return(meta.list)
+}
+
+#Calculate rankings and scores for query data and given signature set
+calculate_Uscore <- function(matrix, features,  maxRank=1500, chunk.size=1000, ncores=1, 
+                             storeRanks=FALSE, force.gc=FALSE, name="_UCell") {
+
+  #Make sure we have a sparse matrix
+  require(Matrix)
+  if (class(matrix) != "dgCMatrix") {
+    matrix <- Matrix::Matrix(as.matrix(matrix,sparse = T))
+  }
+  #Check if all genes in signatures are present in the data matrix
+  matrix <- check_genes(matrix, features)
+
+  #Split into manageable chunks
+  split.data <- split_data.matrix(matrix=matrix, chunk.size=chunk.size)
+
+  #Parallelize?
+  if (ncores>1) {
+    plan(future::multisession(workers=future_param_ncores))
+
+    meta.list <- future_lapply(
+      X = split.data,
+      FUN = function(x) {
+
+        cells_rankings <- data_to_ranks_data_table(x)
+        cells_AUC <- u_stat_signature_list(features, cells_rankings, maxRank=maxRank, sparse=F)
+
+        colnames(cells_AUC) <- paste0(colnames(cells_AUC),name)
+
+        if (storeRanks==T){
+          gene.names <- as.character(as.matrix(cells_rankings[,1]))
+          #make sparse
+          cells_rankings[cells_rankings>maxRank] <- 0
+          ranks.sparse <- Matrix::Matrix(as.matrix(cells_rankings[,-1]),sparse = T)
+          dimnames(ranks.sparse)[[1]] <- gene.names
+          
+          if (force.gc) {
+             cells_rankings <- NULL
+             gc()
+          }
+          return(list(cells_rankings=ranks.sparse, cells_AUC=cells_AUC))
+
+        } else {
+          if (force.gc) {
+            cells_rankings <- NULL
+            gc()
+          }
+          return(list(cells_AUC=cells_AUC))
+        }
+      },
+      future.seed = future_param_seed
+    )
+    plan(strategy = "sequential")
+
+  } else {
+    meta.list <- lapply(
+      X = split.data,
+      FUN = function(x) {
+        cells_rankings <- data_to_ranks_data_table(x)
+        cells_AUC <- u_stat_signature_list(features, cells_rankings, maxRank=maxRank, sparse=F)
+        colnames(cells_AUC) <- paste0(colnames(cells_AUC),name)
+
+        if (storeRanks==T){
+          gene.names <- as.character(as.matrix(cells_rankings[,1]))
+          #make sparse
+          cells_rankings[cells_rankings>maxRank] <- 0
+          ranks.sparse <- Matrix::Matrix(as.matrix(cells_rankings[,-1]),sparse = T)
+          dimnames(ranks.sparse)[[1]] <- gene.names
+          if (force.gc) {
+            cells_rankings <- NULL
+            gc()
+          }
+          return(list(cells_rankings=ranks.sparse, cells_AUC=cells_AUC))
+
+        } else {
+          if (force.gc) {
+            cells_rankings <- NULL
+            gc()
+          }
+          return(list(cells_AUC=cells_AUC))
+        }
+
+      } )
+  }
+  return(meta.list)
+}
