968c78049a9258207c53a7a9cfae18561a1e046c
Massimo Andreatta <massimo.andreatta@unil.ch>
2021-10-28 12:28:50 +0200

    Fix bug occurring when last chunk contains only one cell

--- R/utils.R
+++ R/utils.R
@@ -101,10 +101,16 @@ split_data.matrix <- function(matrix, chunk.size=1000) {
   nchunks <- (ncols-1) %/% chunk.size + 1
 
   split.data <- list()
+  min <- 1
   for (i in 1:nchunks) {
-    min <- 1 + (i-1)*chunk.size
-    max <- min(i*chunk.size, ncols)
+    if (i == nchunks-1) {  #make last two chunks of equal size
+       left <- ncols-(i-1)*chunk.size
+       max <- min+round(left/2)-1
+    } else {
+       max <- min(i*chunk.size, ncols)
+    }
     split.data[[i]] <- matrix[,min:max]
+    min <- max+1    #for next chunk
   }
   return(split.data)
 }
