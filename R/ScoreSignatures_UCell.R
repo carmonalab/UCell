ecf3f290b23c99f92e9a3182a7256b040c3eccff
Massimo Andreatta <massimo.andreatta@unil.ch>
2022-02-07 16:23:22 +0100

    Address interoperability - ScoreSignatures_UCell() takes and returns sce objects

--- R/ScoreSignatures_UCell.R
+++ R/ScoreSignatures_UCell.R
@@ -5,12 +5,13 @@
 #'
 #' These scores only depend on the gene expression ranks of individual cell, and therefore they are robust across datasets regardless of dataset composition.
 #'
-#' @param matrix A gene vs. cell data matrix, either in sparse or dense format. Leave empty if providing a rank matrix with \code{precalc.ranks}
+#' @param matrix Input matrix, either stored in a \code{SingleCellExperiment} object or as a raw matrix. \code{dgCMatrix} format supported.
 #' @param features A list of signatures, for example: \code{list( Tcell_signature = c("CD2","CD3E","CD3D"), Myeloid_signature = c("SPI1","FCER1G","CSF1R"))}
 #'     You can also specify positive and negative gene sets by adding a + or - sign to genes in the signature; see an example below
 #' @param precalc.ranks A sparse matrix of pre-calculated ranks, obtained with \code{\link{StoreRankings_UCell}}
 #' @param maxRank Maximum number of genes to rank per cell; above this rank, a given gene is considered as not expressed.
 #'     Note: this parameter is ignored if \code{precalc.ranks} are specified
+#' @param assay The sce object assay where the data is to be found
 #' @param chunk.size Number of cells to be processed simultaneously (lower size requires slightly more computation but reduces memory demands)
 #' @param w_neg Weight on negative genes in signature. e.g. `w_neg=1` weighs equally up- and down-regulated genes, `w_neg=0.5` gives 50% less importance to negative genes
 #' @param ncores Number of processors to parallelize computation. Requires package \code{future}
@@ -18,32 +19,68 @@
 #' @param name Name suffix appended to signature names
 #' @param force.gc Explicitly call garbage collector to reduce memory footprint
 #' @param seed Integer seed for [future.apply::future_lapply] parallel execution
-#' @return Returns a dataframe of signature scores for each cell
+#' @return Returns input SingleCellExperiment object with UCell scores added to altExp
 #' @examples
 #' ## Not run:
+#' # Using sparse matrix
 #' data(sample.matrix)
 #' gene.sets <- list( Tcell_signature = c("CD2","CD3E","CD3D"),
 #'                  Myeloid_signature = c("SPI1","FCER1G","CSF1R"))
 #' scores <- ScoreSignatures_UCell(sample.matrix, features=gene.sets)
-#' scores[1:5,]
+#' head(scores)
+#' 
+#' # Using sce object
+#' library(SingleCellExperiment)
+#' data(sample.matrix)
+#' my.sce <- SingleCellExperiment(list(counts=sample.matrix))
+#' gene.sets <- list( Tcell_signature = c("CD2","CD3E","CD3D"),
+#'                  Myeloid_signature = c("SPI1","FCER1G","CSF1R"))
+#' my.sce <- ScoreSignatures_UCell(my.sce, features=gene.sets)
+#' altExp(my.sce, 'UCell')
 #' ## End (Not run)
+#' @importFrom methods is 
+#' @import SingleCellExperiment
+#' @importFrom SummarizedExperiment assay SummarizedExperiment
 #' @import Matrix
 #' @export
-ScoreSignatures_UCell <- function(matrix=NULL, features, precalc.ranks=NULL, maxRank=1500, w_neg=1, name="_UCell",
-                                  chunk.size=1000, ncores=1, ties.method="average", force.gc=FALSE, seed=123) {
+ScoreSignatures_UCell <- function(matrix=NULL, features, precalc.ranks=NULL, 
+                                  maxRank=1500, w_neg=1, name="_UCell",
+                                  assay="counts", chunk.size=1000, ncores=1, 
+                                  ties.method="average", force.gc=FALSE, seed=123) {
   
   features <- check_signature_names(features)
   
+  #Check type of input
+  if (methods::is(matrix, "SingleCellExperiment")) { # sce object
+    if (!assay %in% names(matrix@assays)) {
+      stop(sprintf("Assay %s not found in sce object.", assay))
+    }
+    m <- SummarizedExperiment::assay(matrix, assay)
+  } else if (methods::is(matrix, "matrix") | methods::is(matrix, "dgCMatrix")) { #matrix
+    m <- matrix
+  } else {
+    m <- NULL
+  }
+  
+  if (is.null(m) & is.null(precalc.ranks)) {
+    stop("Unrecognized input format.")
+  }
+  
+  #Run on pre-calculated ranks ('m' can be NULL)
   if (!is.null(precalc.ranks)) {
-     meta.list <- rankings2Uscore(precalc.ranks, features=features, chunk.size=chunk.size, w_neg=w_neg,
+     u.list <- rankings2Uscore(precalc.ranks, features=features, chunk.size=chunk.size, w_neg=w_neg,
                                   ncores=ncores, force.gc=force.gc, name=name)
   } else {
-     meta.list <- calculate_Uscore(matrix, features=features, maxRank=maxRank, chunk.size=chunk.size, w_neg=w_neg,
+     u.list <- calculate_Uscore(m, features=features, maxRank=maxRank, chunk.size=chunk.size, w_neg=w_neg,
                                    ties.method=ties.method, ncores=ncores, force.gc=force.gc, name=name)
   }
-  meta.merge <- lapply(meta.list,function(x) rbind(x[["cells_AUC"]]))
-  meta.merge <- Reduce(rbind, meta.merge)
+  u.merge <- lapply(u.list,function(x) rbind(x[["cells_AUC"]]))
+  u.merge <- Reduce(rbind, u.merge)
   
-  return(meta.merge)
-
+  if (methods::is(matrix, "SingleCellExperiment")) {
+     SingleCellExperiment::altExp(matrix, "UCell") <- SummarizedExperiment(assays = list("UCell" = t(u.merge)))
+     return(matrix)
+  } else {
+     return(u.merge)
+  }
 }
--- docs/functions.md
+++ docs/functions.md
@@ -1,8 +1,6 @@
 # UCell - Functions
 
-* `ScoreSignatures_UCell`    Calculate module enrichment scores from single-cell data. Given a gene vs. cell matrix, calculates module/signature enrichment scores on single-cell level using Mann-Whitney U statistic. Returned scores are normalized U statistic (equivalent to AUC - Area Under the Curve). This score depends only on the gene expression ranks of individual cell, and therefore is robust across datasets.
-
-* `ScoreSignatures_UCell_sce`    A wrapper for UCell to interact directly with SingleCellExperiment objects. Give a `sce` object, UCell scores for the specified gene signatures are returned in altExp: `altExp(sce, 'UCell')`
+* `ScoreSignatures_UCell`    Calculate module enrichment scores from single-cell data. Given a gene vs. cell matrix (either as sparse matrix or stored in a `SingleCellExperiment` object), calculates module/signature enrichment scores on single-cell level using Mann-Whitney U statistic. Returned scores are normalized U statistic (equivalent to AUC - Area Under the Curve). This score depends only on the gene expression ranks of individual cell, and therefore is robust across datasets.
 
 * `AddModuleScore_UCell`   A wrapper for UCell to interact directly with Seurat objects. Given a Seurat object, calculates module/signature enrichment scores on single-cell level using Mann-Whitney U statistic. Returned scores are normalized U statistic (equivalent to AUC - Area Under the Curve). In contrast to Seurat's `AddModuleScore` (based on population average gene expression binning) this score depend only on the gene expression ranks of individual cell, and therefore is robust across datasets.
 
